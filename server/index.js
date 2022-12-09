53228ddec03e4b2ed0cabde18c18b0a3119d65a9
cptntz2119 <zhangdong2119@hotmail.com>
2022-12-08 21:51:05 -0800

    minor change on server index.js

--- server/index.js
+++ server/index.js
@@ -1,34 +1,35 @@
 require("dotenv").config();
 
 const PORT = process.env.PORT || 8080;
-const express = require('express');
-const Twilio = require('twilio');
+const express = require("express");
+const Twilio = require("twilio");
 
 var accountSid = process.env.TWILIO_ACCOUNT_SID; // Your Account SID from www.twilio.com/console
-var authToken = process.env.TWILIO_AUTH_TOKEN;   // Your Auth Token from www.twilio.com/console
+var authToken = process.env.TWILIO_AUTH_TOKEN; // Your Auth Token from www.twilio.com/console
 
-const client = require('twilio')(accountSid, authToken);
+const client = require("twilio")(accountSid, authToken);
 
-//Welcome Page for the Server 
-app.get('/', (req, res) => {
-  res.send('Welcome to the Express Server')
-})
+//Welcome Page for the Server
+app.get("/", (req, res) => {
+  res.send("Welcome to the Express Server");
+});
 
-//Twilio 
-app.get('/send-text', (req, res) => {
+//Twilio
+app.get("/send-text", (req, res) => {
   //Welcome Message
-  res.send('Hello to the Twilio Server')
+  res.send("Hello to the Twilio Server");
 
   //_GET Variables
   const { recipient, textmessage } = req.query;
 
-
   //Send Text
-  client.messages.create({
+  client.messages
+    .create({
       body: textmessage,
-      to: recipient,  // Text this number
-      from: '+15074734314' // From a valid Twilio number
-  }).then((message) => console.log(message.body));
-})
+      to: recipient, // Text this number
+      from: "+15627849335", // From a valid Twilio number
+    })
+    .then((message) => console.log(message.body));
+});
 
-app.listen(8080, () => console.log("Running on Port 8080"))
+app.listen(8080, () => console.log("Running on Port 8080"));
